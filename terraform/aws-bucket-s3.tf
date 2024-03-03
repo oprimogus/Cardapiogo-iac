fe96057c5de00deedcd4499861c5126b3023b7ff
Gustavo Ferreira <gustavo.jesus@proton.me>
2024-03-03 19:27:56 -0300

    remove garbage

--- terraform/aws-bucket-s3.tf
+++ terraform/aws-bucket-s3.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "k3s" {
   bucket = "oprimogus-k3s"
   force_destroy = true
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "oprimogus-example"
-  force_destroy = true
-}
