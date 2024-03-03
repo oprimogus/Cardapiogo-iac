2d50ddd8378c154b122303fca07fe10220cab8e3
Gustavo Ferreira <gustavo.jesus@proton.me>
2024-03-03 16:39:01 -0300

    TEST ci

--- terraform/aws-bucket-s3.tf
+++ terraform/aws-bucket-s3.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "oprimogus-terraform-state"
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 resource "aws_s3_bucket" "k3s" {
   bucket = "oprimogus-k3s"
   lifecycle {
